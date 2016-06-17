b0cf205dcf6471a43b2a456980a047bd2eab2e27
Chris Lintecum <clintecum2@gmail.com>
2016-06-17 08:19:55 -0500

    turn off grunt live reload options

--- gruntfile.js
+++ gruntfile.js
@@ -28,48 +28,48 @@ module.exports = function (grunt) {
       serverViews: {
         files: defaultAssets.server.views,
         options: {
-          livereload: true
+          livereload: false
         }
       },
       serverJS: {
         files: _.union(defaultAssets.server.gruntConfig, defaultAssets.server.allJS),
         tasks: ['jshint'],
         options: {
-          livereload: true
+          livereload: false
         }
       },
       clientViews: {
         files: defaultAssets.client.views,
         options: {
-          livereload: true
+          livereload: false
         }
       },
       clientJS: {
         files: defaultAssets.client.js,
         tasks: ['jshint'],
         options: {
-          livereload: true
+          livereload: false
         }
       },
       clientCSS: {
         files: defaultAssets.client.css,
         tasks: ['csslint'],
         options: {
-          livereload: true
+          livereload: false
         }
       },
       clientSCSS: {
         files: defaultAssets.client.sass,
         tasks: ['sass', 'csslint'],
         options: {
-          livereload: true
+          livereload: false
         }
       },
       clientLESS: {
         files: defaultAssets.client.less,
         tasks: ['less', 'csslint'],
         options: {
-          livereload: true
+          livereload: false
         }
       }
     },
